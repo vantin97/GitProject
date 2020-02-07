0a5d2f8960234cc9c5771b8f228c021ad3f02e0f
tin <tin11011997@gmail.com>
2020-02-07 11:28:06 +0700

    .

---
+++ BC NETCORE/Sử dụng toán tử/sudungtoantu.js
@@ -0,0 +1,8 @@
+var inputWidth;
+var inputHeight;
+inputWidth= prompt('gia tri chieu rong');
+inputHeight= prompt('gia tri chieu dai');
+Width=parseInt(inputWidth);
+Height=parseInt(inputHeight);
+var area  = Width * Height;
+document.write("Diện tích là: " + area);
