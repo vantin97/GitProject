89782824e7dc1e1fc61ec6c3da80392f215acdf7
tin <tin11011997@gmail.com>
2020-02-26 11:24:37 +0700

    .

---
+++ BC NETCORE/String/a.js
@@ -0,0 +1,8 @@
+function isFirstLetterUpperCase(str){
+    regexp = /^[A-Z]/;
+    if (regexp.test(str)) {
+       document.write("String's first character is uppercase");
+    } else {
+       document.write("String's first character is not uppercase");
+    }
+ }
